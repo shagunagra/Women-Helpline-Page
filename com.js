ea20cb7284778bb84036acf9a405243e7b99e3be
shagunagra <59168880+shagunagra@users.noreply.github.com>
2020-04-25 15:23:07 -0700

    javascript file of home page

---
+++ com.js
@@ -0,0 +1,8 @@
+function terms() {
+    var temp= '<p>If the registered case will be false a strict action will be taken against the person.</p>' +
+    '<p>Do not fill false details as it will be difficult to help you</p>';
+    document.getElementById('conditions').innerHTML = temp;
+    document.getElementById('conditions').style.color='blue';
+    document.getElementById('conditions').style.fontWeight='bolder';
+    document.getElementById('conditions').style.padding= '2%';
+}
